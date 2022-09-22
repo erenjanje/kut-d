fcbb63940ef1ea36745dea9b3598b3e416b47bec
erencan-c <erencan.ceyhan@hotmail.com>
2022-09-22 18:51:25 +0300

    Support for reading from files

--- source/app.d
+++ source/app.d
@@ -1,16 +1,22 @@
 import std.stdio;
+import std.file;
+import std.conv : to;
 import kut.parser;
 import kut.interpreter;
 import kut.console_screen;
 
-void main() {
-   Token[] tokens = parseKut(`(ekran "aa" yazsın)`);
+void main(string[] args) {
+   if(args.length < 2) {
+      throw new Error("Not enough arguments!");
+   }
+   string filename = args[1];
+   //File file = File(filename, "r");
+   string data = read(filename).to!string;
+   Token[] tokens = data.to!dstring.parseKut;
    KutObject[dstring] immutableVariables = [
       "ekran": KutObject.externalObject(new KutScreen()),
    ];
    KutObject[dstring] variables;
    auto ctx = new KutContext(immutableVariables, variables);
-   //writeln(ctx.variables);
    ctx.evaluate(tokens);
-   //writeln(ctx.variables["ananza"]);
 }
